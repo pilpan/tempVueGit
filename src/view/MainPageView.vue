f47eb8b42a91fd1ea8528a66d9f32d979f40a1b4
plip <plip2010195@yandex.ru>
2023-10-10 16:12:02 +0300

    add gitCommand

--- src/view/MainPageView.vue
+++ src/view/MainPageView.vue
@@ -1,10 +1,21 @@
 <template lang="">
     <div class='activeCard'>
         Main Page 
+        <div v-bind:style="{backgroundColor: redBox.backgroundColor}">123</div>
     </div>
+
 </template>
 <script>
 export default {
+    data() {
+        return {
+            redBox:{
+                height: '40px',
+                width: '40px',
+                backgroundColor: 'Red',
+            }
+        }
+    },
 
 }
 </script>
@@ -12,5 +23,11 @@ export default {
 .activeCard {
     background-color: bisque;
     color: yellow;
+}
+
+.redBox {
+    width: 40px;
+    height: 40px;
+    background-color: red;
 }
 </style>
